c698dde036f117993dcb4cafcee85b0c45d2b1f1
hizuraky <hizuraky@gmail.com>
2022-11-19 19:58:02 +0900

    Add feature auth

---
+++ src/features/auth/routes/Login.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Layout } from '../components/Layout';
+
+export const Login = () => {
+  return (
+    <Layout title="Log in to your account">
+      <div />
+    </Layout>
+  );
+};
---
+++ src/features/auth/routes/Register.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Layout } from '../components/Layout';
+
+export const Register = () => {
+  return (
+    <Layout title="Register your account">
+      <div />
+    </Layout>
+  );
+};
